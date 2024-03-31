b9e57bdb5a25656cbe82e5e684b9c635948c6ae7
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-03-31 18:07:38 +0200

    Remove unused libs on package json. Modify the email name of the app

--- utils/reset-password-mail.ts
+++ utils/reset-password-mail.ts
@@ -15,14 +15,14 @@ export const handleResetPasswordMail = ({
   const emailData = {
     to: receiverMail,
     from: SENDER_MAIL_ACC ?? "",
-    subject: `Reset Your Password on Expense Manager`,
+    subject: `Reset Your Password on Expense Tracker`,
     html: `
           <p>Hello,</p>
-          <p>You have requested to reset your password on Expense Manager. Click the button below to proceed:</p>
+          <p>You have requested to reset your password on Expense Tracker. Click the button below to proceed:</p>
           <a href="${APP_BASE_URL}/auth/reset?token=${token}" style="background-color: #007bff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;" target="_blank" rel="noopener noreferrer">Reset Password</a>
           <p>This link will expire in 1 hour. If you did not request a password reset, please ignore this email or contact us at <a href="mailto:info@pabloaviles.es">info@pabloaviles.es</a>.</p>
           `,
-    text: `You have requested to reset your password on Expense Manager. Please copy and paste the following link into your browser: ${APP_BASE_URL}/auth/recovery?token=${token} This link will expire in 1 hour. If you did not request a password reset, please ignore this email or contact us at info@pabloaviles.es.`,
+    text: `You have requested to reset your password on Expense Tracker. Please copy and paste the following link into your browser: ${APP_BASE_URL}/auth/recovery?token=${token} This link will expire in 1 hour. If you did not request a password reset, please ignore this email or contact us at info@pabloaviles.es.`,
   };
   return sgMail.send(emailData);
 };
