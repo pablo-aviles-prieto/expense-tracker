eea3664a0e7102e0157835e453093a45c8aa3f7e
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-04-01 22:10:47 +0200

    Add images from pexel and test them

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,6 +6,8 @@ export const metadata: Metadata = {
   description: "Signin or signup into expense tracker",
 };
 
+// TODO: Add random quotes and images of stock on the left side
+// TODO: Add the logo in top of the login/signup panels
 export default async function AuthLayout({
   children,
 }: {
@@ -13,19 +15,26 @@ export default async function AuthLayout({
 }) {
   return (
     <div className="relative flex-col items-center justify-center h-screen md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative flex-col hidden h-full p-10 text-white bg-muted dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium gap-x-2">
+      <div className="relative flex-col hidden h-full p-10 text-white dark:border-r lg:flex">
+        <div
+          className="absolute inset-0 bg-zinc-900"
+          style={{
+            backgroundImage: `url('/images/auth-images/image1.jpg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <div className="relative z-20 flex items-center text-lg font-medium gap-x-2 p-0.5 bg-black bg-opacity-20 rounded-lg w-[200px]">
           <Image
             src="/images/logo.webp"
             alt="Expense tracker logo"
             width={45}
             height={45}
           />
-          Expense tracker
+          <span className="">Expense tracker</span>
         </div>
         <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
+          <blockquote className="p-2 space-y-2 bg-black bg-opacity-50 rounded-lg">
             <p className="text-lg">
               &ldquo;This library has saved me countless hours of work and
               helped me deliver stunning designs to my clients faster than ever
