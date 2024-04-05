a81c75936721c206eba262ec7635aa59b0963eab
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-04-06 00:34:14 +0200

    Add metadata

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -7,9 +7,6 @@ export const metadata: Metadata = {
   description: "Manage your expenses and incomes easily!",
 };
 
-// TODO: Add a readme
-// TODO: Add some metada data
-// TODO: Check not leaking any key into the client!
 export default function DashboardLayout({
   children,
 }: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,48 @@ import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// TODO: Add some metada data
+// TODO: Add a readme
+// TODO: Check not leaking any key into the client!
+// TODO: Create a sample video
+
 export const metadata: Metadata = {
-  title: "Expense tracker",
-  description: "Manage your expenses and incomes easily!",
+  title: "Expense Tracker - Simplify Your Financial Management",
+  description:
+    "Expense Tracker is an intuitive app designed to help you manage your finances effortlessly. Keep track of your incomes, expenses, and subscriptions, analyze your financial habits, and make informed decisions. Featuring a user-friendly dashboard, transaction filtering, bulk CSV uploads, and subscription management.",
+  keywords: [
+    "expense tracker",
+    "financial management",
+    "budgeting",
+    "income tracking",
+    "expense tracking",
+    "subscription management",
+  ],
+  authors: [{ name: "Pablo Avilés Prieto", url: "https://www.pabloaviles.es" }],
+  viewport: "width=device-width, initial-scale=1",
+  applicationName: "Expense Tracker",
   icons: [{ rel: "icon", url: "/images/favicon.ico" }],
+  openGraph: {
+    type: "website",
+    url: "https://www.expense-tracker.pabloaviles.es",
+    title: "Manage Your Finances with Ease | Expense Tracker",
+    description:
+      "Track your incomes and expenses, manage subscriptions, and get insights into your financial habits with Expense Tracker. Start simplifying your financial management today.",
+    images: [
+      {
+        url: "https://www.expense-tracker.pabloaviles.es/images/social-networks-preview.png",
+      },
+    ],
+    siteName: "Expense Tracker",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Manage Your Finances with Ease | Expense Tracker",
+    description:
+      "Track your incomes and expenses, manage subscriptions, and get insights into your financial habits with Expense Tracker. Start simplifying your financial management today.",
+    images:
+      "https://www.expense-tracker.pabloaviles.es/images/social-networks-preview.png",
+  },
 };
 
 export default async function RootLayout({
