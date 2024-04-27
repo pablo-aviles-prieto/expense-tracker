c6bd1a15bdf3e76deec6189b0f8c978f96eae2f8
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-04-27 17:55:14 +0200

    Remove mongodb_uri environment variable to avoid error when mounting docker image

--- lib/mongodb-config.ts
+++ lib/mongodb-config.ts
@@ -12,21 +12,21 @@ const globalWithMongo = global as typeof globalThis & {
   _mongoClientPromise: Promise<MongoClient>;
 };
 
-if (!process.env.MONGODB_URI || !uri) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
+// if (!uri) {
+//   throw new Error("Please add your Mongo URI to .env.local");
+// }
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri ?? "", options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri ?? "", options);
   clientPromise = client.connect();
 }
 
