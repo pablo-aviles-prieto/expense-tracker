356b12d7cea8fdc534f311bd9e28c949887b5b44
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-10-06 14:42:29 +0200

    WIP hero section

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -5,15 +5,16 @@ import { Typography } from '../ui/typography';
 
 export const Header = () => {
   return (
-    <header className='h-16 border-b'>
+    <header className='h-16'>
       <div className='mx-auto flex h-full max-w-7xl items-center justify-between px-2 xl:px-0'>
         <Image src='/images/logo.webp' alt='Expense tracker logo' width={45} height={45} />
         <div className='flex items-center gap-x-4'>
-          <Typography variant='h4' className='flex items-center gap-x-2'>
-            <Icons.login className='size-4' /> Login
+          <Typography variant='h5' className='flex items-center gap-x-2'>
+            <Icons.login className='size-4' /> Log in
           </Typography>
-          <Typography variant='h4' className='flex items-center gap-x-2'>
-            <Icons.signup className='size-4' /> Register
+          {/* Convert this into a button and remove icons like fanfix */}
+          <Typography variant='h5' className='flex items-center gap-x-1'>
+            <Icons.signup className='size-4' /> Sign up
           </Typography>
         </div>
       </div>
--- components/landing-page/hero-section/action-button.tsx
+++ components/landing-page/hero-section/action-button.tsx
@@ -10,7 +10,7 @@ export async function ActionButton() {
       <AnimatedGradientText>
         <span
           className={cn(
-            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text px-2 py-1 text-transparent`
+            `inline animate-gradient bg-gradient-to-r from-[#00e60c] via-[#9c40ff] to-[#00e60c] bg-[length:var(--bg-size)_100%] bg-clip-text px-2 py-1 text-base text-transparent`
           )}
         >
           Start growing your wealth
