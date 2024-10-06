cb4d08a423b0b992adb2a0718fbe671c94a2089d
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-10-06 12:07:43 +0200

    add eslint-plugin-tailwindcss

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -10,10 +10,10 @@ export const Header = () => {
         <Image src='/images/logo.webp' alt='Expense tracker logo' width={45} height={45} />
         <div className='flex items-center gap-x-4'>
           <Typography variant='h4' className='flex items-center gap-x-2'>
-            <Icons.login className='h-4 w-4' /> Login
+            <Icons.login className='size-4' /> Login
           </Typography>
           <Typography variant='h4' className='flex items-center gap-x-2'>
-            <Icons.signup className='h-4 w-4' /> Register
+            <Icons.signup className='size-4' /> Register
           </Typography>
         </div>
       </div>
