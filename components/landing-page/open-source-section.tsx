0d488795a1174514e8d5f47125f8fcacfb542e5a
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2024-11-04 18:05:16 +0100

    Tilted image of the repo

--- components/landing-page/open-source-section.tsx
+++ components/landing-page/open-source-section.tsx
@@ -1,52 +1,24 @@
-'use client';
-
-import { useRef } from 'react';
-
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-
 import { Typography } from '../ui/typography';
 import { SectionBlock } from './section-block-wrapper';
 
 export const OpenSourceSection = () => {
-  const anchorRef = useRef(null);
-
-  useGSAP(() => {
-    gsap.fromTo(
-      anchorRef.current,
-      { skewY: 0 }, // Initial skew
-      {
-        skewY: -1, // Final skew
-        duration: 1.5,
-        ease: 'power2.out',
-        scrollTrigger: {
-          trigger: anchorRef.current,
-          start: 'top 60%',
-          once: true,
-          markers: true,
-        },
-      }
-    );
-  }, [anchorRef]);
-
   return (
-    <SectionBlock title='open source code'>
-      <div className='mx-auto w-full md:w-3/4 xl:w-3/5'>
-        <Typography variant='h5' className='text-pretty'>
+    <SectionBlock title='open source code' className='px-0'>
+      <div className='mx-auto w-full'>
+        <Typography variant='h5' className='container text-pretty'>
           The code is fully open-source, allowing complete transparency into how the app works
           behind the scenes. You’re welcome to explore the repository, make suggestions, and
           contribute through pull requests or issues. Let’s build a better tool together!
         </Typography>
-        <div className='mx-4 rounded-lg bg-purple-950 md:mx-12'>
+        <div className='titled-image-container h-[300px] rounded-lg contain-strict md:h-[550px] xl:h-[700px]'>
           <a
             href='https://github.com/pablo-aviles-prieto/expense-tracker'
             target='_blank'
             rel='noopener noreferrer'
-            className='mt-8 block'
-            ref={anchorRef}
+            className='tilted-image relative block w-[800px] rounded-lg md:w-full'
           >
             <img
-              className='w-full rounded-lg shadow-sm shadow-purple-300'
+              className='mt-24 w-full rounded-lg md:mt-36 xl:mt-[160px]'
               src='/images/landing/repo.webp'
               alt='Expense tracker github repository'
             />
