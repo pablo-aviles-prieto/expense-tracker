20994a182d856575470d9f8be5e8e0409641ac91
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2026-05-25 01:20:57 +0200

    update crton timer

--- services/subscription-notification/notification.job.ts
+++ services/subscription-notification/notification.job.ts
@@ -29,7 +29,7 @@ export type NotificationData = z.infer<typeof notificationDataSchema>;
 
 class SubscriptionNotificationJob {
   private schedules = {
-    midnight: '20 01 * * *',
+    midnight: '28 01 * * *',
   };
 
   private jobs: Map<string, { task: ScheduledTask; schedule: string }> = new Map();
